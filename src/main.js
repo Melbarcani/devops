e7dcd917e7049141de160c782b825ca002258cb8
Melbarcani <48389494+Melbarcani@users.noreply.github.com>
2020-12-27 01:55:10 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
 Vue.config.productionTip = false
 Vue.use(VueMaterial)
-
+/**/
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
